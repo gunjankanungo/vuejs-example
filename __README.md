9c9d2c6354e450945fe88a9a0eef7f571b8362b7
gunjankanungo <gunjan.kanungo321@gmail.com>
2017-10-26 16:48:15 +0530

    First Commit

---
+++ __README.md
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+# github-user-search
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+=======
+# vuejs-example
+Vue Js Example
+>>>>>>> 5dd9c8f967243443f1deb9a38d8dd06adb8d21f2
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import HolaMundo from '@/components/HolaMundo'
+import GitHub from '@/components/GitHub'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'GitHub',
+      component: GitHub
+    },
+    {
+      path: '/es',
+      name: 'Hola',
+      component: HolaMundo
+    },
+    {
+      path: '/en',
+      name: 'hello',
+      component: HelloWorld
+    }
+  ]
+})
